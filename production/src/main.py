5b710968e6fe8b96aada7ddc0e8dce6261f2a70e
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-10-28 18:21:44 +0200

    [FIX] Correct the logger handler [#9]

--- production/src/main.py
+++ production/src/main.py
@@ -1,38 +1,26 @@
 import logging
 from logging.handlers import RotatingFileHandler
-import time
-# import pilotLED
-
+import sys
 
 def __configureLogger() -> None:
     """Configure the app's logger
 
-    The log file is stored in `ClearWay.log` and its maximum size is 5MB, every time the program
+    The log file is stored in `ClearWay.log` and every time the program
     restart, the file is cleared.
     The minimum level of the lof is `INFO`.
     Is format is:
-        `Date Time Level FunctionName LineNumber Message`
+        `Date Time FileName:FunctionName Level >> Message`
         example:
-            `2021-10-28 16:42:01,393 INFO foo(23) Test Logger`
+            `2021-10-28 18:20:19,018 foo.py:10 [INFO] >> Foo`
     """
-
-    logFormatter = logging.Formatter("%(asctime)s %(levelname)s %(funcName)s-%(lineno)d : %(message)s")
-    appLog = logging.getLogger('root')
-    appLog.setLevel(logging.INFO)  # TODO On Realease change to INFO
-
-
-    # TODO On Realease remove
-    # create console handler and set level to info
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.DEBUG)
-    consoleHandler.setFormatter(logFormatter)
-    appLog.addHandler(consoleHandler)
-
-    # create file handler and set level to info
-    fileHandler = RotatingFileHandler(mode='w', maxBytes=5*1024*1024, encoding="utf8", filename="ClearWay.log")
-    fileHandler.setFormatter(logFormatter)
-    fileHandler.setLevel(logging.DEBUG)  # TODO On Realease change to INFO
-    appLog.addHandler(fileHandler)
+    logging.basicConfig(
+        level=logging.DEBUG,  # TODO On Realease change to INFO
+        format="%(asctime)s [%(filename)s:%(lineno)d] %(levelname)s >> %(message)s",
+        handlers=[
+            logging.FileHandler("ClearWay.log"),
+            logging.StreamHandler(sys.stdout)  # TODO On Realease remove
+        ],
+    )
 
 
 def main():
