96be470fd2900ec5f5b421e90ff4a9d979413059
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-10-29 12:52:37 +0200

    [FEAT] Implement asynchronous state machine to pilot the GPIO [#9]

--- production/src/main.py
+++ production/src/main.py
@@ -1,26 +1,39 @@
 import logging
-from logging.handlers import RotatingFileHandler
+import pilotLED
 import sys
+import time
+
 
 def __configureLogger() -> None:
     """Configure the app's logger
 
     The log file is stored in `ClearWay.log` and every time the program
     restart, the file is cleared.
-    The minimum level of the lof is `INFO`.
     Is format is:
         `Date Time FileName:FunctionName Level >> Message`
         example:
             `2021-10-28 18:20:19,018 foo.py:10 [INFO] >> Foo`
+
+    There is two mode:
+        - Release mode, where the log are only print in the file, the minimum level of the lof is `INFO`.
+        - Debug mode, where the log are print in the file and displayed in the console, The minimum level of the lof is `DEBUG`.
     """
+
+    if __debug__:
     logging.basicConfig(
-        level=logging.DEBUG,  # TODO On Realease change to INFO
+            level=logging.DEBUG,
         format="%(asctime)s [%(filename)s:%(lineno)d] %(levelname)s >> %(message)s",
         handlers=[
             logging.FileHandler("ClearWay.log"),
-            logging.StreamHandler(sys.stdout)  # TODO On Realease remove
+                logging.StreamHandler(sys.stdout)
         ],
     )
+    else:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s [%(filename)s:%(lineno)d] %(levelname)s >> %(message)s",
+            filename="ClearWay.log"
+        )
 
 
 def main():
