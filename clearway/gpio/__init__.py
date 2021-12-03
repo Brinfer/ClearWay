29d25e863c1c84fbb189428e6e481c4f44553e56
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-12-03 11:23:19 +0100

    [FIX] Move the RPI.GPIO import to the module level [#9]

--- clearway/gpio/__init__.py
+++ clearway/gpio/__init__.py
@@ -1,1 +1,24 @@
 """Module to control the inputs and outputs of the card."""
+
+"""Variable linking to the package RPi.GPIO."""
+GPIO = None
+
+
+def use_gpio(p_value: bool) -> None:
+    """Tells the rest of the module if they can modify the GPIO level.
+
+    If the passed parameter is `True`, then the module `RPi.GPIO` is imported.
+    This function must be called at the beginning of the program.
+
+    Parameters
+    ----------
+    p_value : `bool`
+        `True` if you want to use GPIOs, `False` otherwise.
+    """
+    global GPIO
+
+    if p_value is True:
+        # Import RPi.GPIO and save it in a global variable
+        import RPi.GPIO as GPIO  # noqa: N814: camelcase 'RPi.GPIO' imported as constant 'GPIO'
+    else:
+        GPIO = None
