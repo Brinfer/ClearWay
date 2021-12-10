d1a2e75547eb3ebed8bca57b662a8bb716b1a587
Chauvin Léo <leo_chauvin@reseau.eseo.fr>
2021-12-10 10:51:10 +0100

    [FEAT] Use of a class for the ai file to get the proper detection back [#17]

--- clearway/ai/ai.py
+++ clearway/ai/ai.py
@@ -11,7 +11,7 @@ from clearway.gpio import stateMachinePanel
 
 
 @unique
-class __IdYoloOutputLayer(IntEnum):
+class _IdYoloOutputLayer(IntEnum):
     """ID related to the three output layers of YOLO.
 
     The first four ID represent the location of detected objects.
@@ -27,165 +27,186 @@ class __IdYoloOutputLayer(IntEnum):
     BICYCLE = auto()
 
 
-__object_detection_id = __IdYoloOutputLayer.BICYCLE
-__network = None
-__output_layers = []
-__video_stream = None
-__path_to_input_video = None
-__path_to_output_video = None
-__output_video = None
-__output_color = (0, 0, 255)  # Blue
-__prob_threshold = 0.5
-__detect = False
-__detect_old = False
+class Ai:
+    """[summary].
 
-
-def init(yolo_weights, yolo_cfg, path_to_input_video=None, path_to_output_video=None) -> None:
-    """Get the three output layers of YOLO and start the video stream.
-
-    Parameters
-    ----------
-    path_to_input_video : string, optional
-        The path to the input video that is going to be processed, by default None.
+    Returns
+    -------
+    [type]
+        [description]
     """
-    global __network, __output_layers, __path_to_input_video, __path_to_output_video, __video_stream, __output_video
 
-    # Read the deep learning network Yolo
-    __network = cv2.dnn.readNet(yolo_weights, yolo_cfg)
-    # Find names of all layers of the YOLO model architecture
-    layer_names = __network.getLayerNames()
-    __output_layers = [layer_names[i[0] - 1] for i in __network.getUnconnectedOutLayers()]
+    # Class variables shared by all instances
+    __object_detection_id = _IdYoloOutputLayer.BICYCLE
+    __output_color = (0, 0, 255)  # Blue
+    __prob_threshold = 0.5
+    __object_detection_counter = 0
+    __font = cv2.FONT_HERSHEY_DUPLEX
 
-    if path_to_input_video is None:
-        __video_stream = VideoStream(usePiCamera=True).start()
-        # Very important! Otherwise, video_stream.read() gives a NonType
-        time.sleep(2.0)
-        logging.info("[AI] Camera ready to detect")
-    else:
-        __path_to_input_video = path_to_input_video
-        __video_stream = cv2.VideoCapture(path_to_input_video)
-        # __video_stream = cv2.VideoCapture("/dev/video0")
+    def __init__(self, yolo_weights, yolo_cfg, path_to_input_video=None, path_to_output_video=None) -> None:
+        """Get the three output layers of YOLO and start the video stream.
 
-    if path_to_output_video is not None:
-        # execution_path = os.getcwd()
-        __path_to_output_video = path_to_output_video
-        output_video_file = os.path.join(path_to_output_video, "video_processed.mp4")
-        x_shape = int(__video_stream.get(cv2.CAP_PROP_FRAME_WIDTH))
-        y_shape = int(__video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        four_cc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
-        __output_video = cv2.VideoWriter(output_video_file, four_cc, fps=1, frameSize=(x_shape, y_shape))
+        Parameters
+        ----------
+        path_to_input_video : string, optional
+            The path to the input video that is going to be processed, by default None.
+        """
+        # Read the deep learning network Yolo
+        self.__network = cv2.dnn.readNet(yolo_weights, yolo_cfg)
+        # Find names of all layers of the YOLO model architecture
+        layer_names = self.__network.getLayerNames()
+        self.__output_layers = [layer_names[i[0] - 1] for i in self.__network.getUnconnectedOutLayers()]
 
+        self.__path_to_input_video = path_to_input_video
+        self.__path_to_output_video = path_to_output_video
+        self.__detect_old = False
 
-def bicycle_detector(gpio_led) -> None:
-    """Detect cyclists on a video stream frame by frame.
-
-    Get the video stream from the Raspberry Pi camera.
-    Process the stream to detect cyclists using a YOLO algorithm and the openCV library.
-    """
-    # Start the frames per second
-    fps = FPS().start()
-
-    if __path_to_input_video is None:
-        read_ok = True
-        img = __video_stream.read()
-    else:
-        read_ok, img = __video_stream.read()
-
-    while read_ok:
-        # Get dimensions of image
-        height, width, channels = img.shape
-
-        # Convert image to Blob
-        # Scalefactor of 1/255 to scale the pixel values to [0..1]
-        blob = cv2.dnn.blobFromImage(img, scalefactor=1 / 255, size=(416, 416), mean=(0, 0, 0), swapRB=True, crop=False)
-        # Set input for YOLO object detection
-        __network.setInput(blob)
-        # Send Blob image data to the three output layers
-        outs = __network.forward(__output_layers)
-
-        confidences = []
-        boxes = []
-
-        for out in outs:
-            for detection in out:
-                confidence = detection[__object_detection_id]
-                if confidence > __prob_threshold:
-                    # Object detected
-                    center_x = int(detection[__IdYoloOutputLayer.CENTER_X] * width)
-                    center_y = int(detection[__IdYoloOutputLayer.CENTER_Y] * height)
-                    w = int(detection[__IdYoloOutputLayer.WIDTH] * width)
-                    h = int(detection[__IdYoloOutputLayer.HEIGHT] * height)
-                    # Rectangle coordinates
-                    x = int(center_x - w / 2)
-                    y = int(center_y - h / 2)
-                    boxes.append([x, y, w, h])
-                    confidences.append(float(confidence))
-
-        # To remove multiple boxes that refer to the same object and keep one by Non Maximum Supression
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, score_threshold=0.5, nms_threshold=0.4)
-
-        img_processed = dram_boxes_and_call_state_machine(indexes, boxes, confidences, img, gpio_led)
-
-        # Update the FPS counter
-        fps.update()
-
-        if __path_to_input_video is None:
-            img = __video_stream.read()
+        if self.__path_to_input_video is None:
+            self.__video_stream = VideoStream(usePiCamera=True).start()
+            # Very important! Otherwise, video_stream.read() gives a NonType
+            time.sleep(2.0)
+            logging.info("[AI] Camera ready to detect")
         else:
-            read_ok, img = __video_stream.read()
+            self.__video_stream = cv2.VideoCapture(path_to_input_video)
+            # self.__video_stream = cv2.VideoCapture("/dev/video0")
 
-        if __path_to_output_video is not None:
-            __output_video.write(img_processed)
+        if self.__path_to_output_video is not None:
+            output_video_file = os.path.join(path_to_output_video, "video_processed.mp4")
+            x_shape = int(self.__video_stream.get(cv2.CAP_PROP_FRAME_WIDTH))
+            y_shape = int(self.__video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            four_cc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
+            self.__output_video = cv2.VideoWriter(output_video_file, four_cc, fps=15, frameSize=(x_shape, y_shape))
 
-    # Stop the timer and display FPS information
-    fps.stop()
-    logging.debug("[IA] elapsed time: {:.2f}".format(fps.elapsed()))
-    logging.debug("[IA] approx. FPS: {:.2f}".format(fps.fps()))
+    def bicycle_detector(self, gpio_led) -> None:
+        """Detect cyclists on a video stream frame by frame.
 
-    if __path_to_input_video is None:
-        __video_stream.stop()
+        Get the video stream from the Raspberry Pi camera.
+        Process the stream to detect cyclists using a YOLO algorithm and the openCV library.
+        """
+        start_time = time.time()
 
+        # Start the frames per second
+        fps = FPS().start()
 
-def dram_boxes_and_call_state_machine(indexes, boxes, confidences, img, gpio_led):
-    """Draw boxes around object detected and inform the gpio stateMachinePanel if a cyclist is detected or not.
+        if self.__path_to_input_video is None:
+            read_ok = True
+            img = self.__video_stream.read()
+        else:
+            read_ok, img = self.__video_stream.read()
 
-    Parameters
-    ----------
-    indexes : [[int]]
-        List of object indexes used to remove multiple boxes that refer to the same object.
-    boxes : [[int]]
-        List of boxes with their information (center_x, center_y, width, height).
-    confidences : int[]
-        List of detection confidences concerning objects on the image being processed.
-    img : numpy.ndarray
-        The image being processed.
-    """
-    global __detect, __detect_old
-    font = cv2.FONT_HERSHEY_DUPLEX
-    __detect = len(boxes) != 0
-    # If something is newly detected (rising edge)
-    if __detect and not __detect_old:
-        for i in range(len(boxes)):
-            if i in indexes:
-                logging.info(
-                    "[AI] {} detected with a probability of: {:.2f} %".format(
-                        __object_detection_id.name, confidences[i] * 100
+        while read_ok:
+            # Get dimensions of image
+            height, width, channels = img.shape
+
+            # Convert image to Blob
+            # Scalefactor of 1/255 to scale the pixel values to [0..1]
+            blob = cv2.dnn.blobFromImage(
+                img, scalefactor=1 / 255, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False
+            )
+            # Set input for YOLO object detection
+            self.__network.setInput(blob)
+            # Send Blob image data to the three output layers
+            outs = self.__network.forward(self.__output_layers)
+
+            confidences = []
+            boxes = []
+
+            # Table of 85 columns for the 3 output layers: outs[0] (507 rows), outs[1] (2028 r) et outs[2] (8112 r)
+            # For each row: Cx, Cy, w, h, confidence + probability for each class
+            for out in outs:
+                for detection in out:
+                    confidence = detection[Ai.__object_detection_id]
+                    if confidence > Ai.__prob_threshold:
+                        # Object detected
+                        center_x = int(detection[_IdYoloOutputLayer.CENTER_X] * width)
+                        center_y = int(detection[_IdYoloOutputLayer.CENTER_Y] * height)
+                        w = int(detection[_IdYoloOutputLayer.WIDTH] * width)
+                        h = int(detection[_IdYoloOutputLayer.HEIGHT] * height)
+                        # Rectangle coordinates
+                        x = int(center_x - w / 2)
+                        y = int(center_y - h / 2)
+                        boxes.append([x, y, w, h])
+                        confidences.append(float(confidence))
+
+            # To remove multiple boxes that refer to the same object and keep one by Non Maximum Supression
+            indexes = cv2.dnn.NMSBoxes(boxes, confidences, score_threshold=0.5, nms_threshold=0.4)
+
+            img = Ai.dram_boxes_and_call_state_machine(self, indexes, boxes, confidences, img, gpio_led)
+
+            # Update the FPS counter
+            fps.update()
+
+            cv2.imshow("Image", img)
+            # Close video window by pressing 'x'
+            if cv2.waitKey(1) & 0xFF == ord("x"):
+                break
+
+            # Write the image to the output video
+            if self.__path_to_output_video is not None:
+                self.__output_video.write(img)
+
+            # Read the next frame
+            if self.__path_to_input_video is None:
+                img = self.__video_stream.read()
+            else:
+                read_ok, img = self.__video_stream.read()
+
+        # Stop the timer and display FPS information
+        fps.stop()
+        program_time = time.time() - start_time
+        logging.debug("--- {:.2f} seconds ---".format(program_time))
+        logging.debug("[IA] elapsed time: {:.2f}".format(fps.elapsed()))
+        logging.debug("[IA] approx. FPS: {:.2f}".format(fps.fps()))
+        logging.debug("[IA] Nb of object detected: " + str(Ai.__object_detection_counter))
+
+        if self.__path_to_input_video is None:
+            self.__video_stream.stop()
+
+    def dram_boxes_and_call_state_machine(self, indexes, boxes, confidences, img, gpio_led):
+        """Draw boxes around object detected and inform the gpio stateMachinePanel if a cyclist is detected or not.
+
+        Parameters
+        ----------
+        indexes : [[int]]
+            List of object indexes used to remove multiple boxes that refer to the same object.
+        boxes : [[int]]
+            List of boxes with their information (center_x, center_y, width, height).
+        confidences : int[]
+            List of detection confidences concerning objects on the image being processed.
+        img : numpy.ndarray
+            The image being processed.
+        """
+        detect = len(boxes) != 0
+        # If something is newly detected (rising edge)
+        if detect and not self.__detect_old:
+            Ai._detect_old = True
+            for i in range(len(boxes)):
+                if i in indexes:
+                    Ai.__object_detection_counter += 1
+                    logging.info(
+                        "[AI] {} detected with a probability of: {:.2f} %".format(
+                            Ai.__object_detection_id.name, confidences[i] * 100
+                        )
                     )
-                )
 
-                stateMachinePanel.signal(gpio_led)
+                    stateMachinePanel.signal(gpio_led)
 
-                if __path_to_output_video is not None:
-                    x, y, w, h = boxes[i]
-                    confidence_label = int(confidences[i] * 100)
-                    cv2.rectangle(img, (x, y), (x + w, y + h), __output_color, 2)
-                    cv2.putText(
-                        img, f"{__object_detection_id.name, confidence_label}", (x - 25, y + 75), font, 2,
-                        __output_color, 2
-                    )
-    # Else if something is not detected but it was before (falling edge)
-    elif __detect_old and not __detect:
-        stateMachinePanel.end_signal(gpio_led)
-    __detect_old = __detect
+                    if self.__path_to_output_video is not None:
+                        x, y, w, h = boxes[i]
+                        confidence_label = int(confidences[i] * 100)
+                        cv2.rectangle(img, (x, y), (x + w, y + h), Ai.__output_color, 2)
+                        cv2.putText(
+                            img,
+                            f"{Ai.__object_detection_id.name, confidence_label}",
+                            (x - 25, y + 75),
+                            Ai.__font,
+                            2,
+                            Ai.__output_color,
+                            2,
+                        )
+        # Else if something is not detected but it was before (falling edge)
+        elif self.__detect_old and not detect:
+            self.__detect_old = False
+            stateMachinePanel.end_signal(gpio_led)
 
-    return img
+        return img
