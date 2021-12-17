19fb7795575cda6d0c1fcd772d0899e807e51642
frissada <damien.frissant@reseau.eseo.fr>
2021-12-17 09:57:33 +0100

    [FEAT] Use event to delay the end signal [#18]

--- clearway/ai/ai.py
+++ clearway/ai/ai.py
@@ -1,10 +1,6 @@
 """OpenCV object detection using picamera."""
 import time
-
-# from threading import Event  # Needed for the  wait() method
-import threading
-
-# from threading import Timer
+from threading import Event  # Needed for the  wait() method
 import logging
 from enum import IntEnum, auto, unique
 import os
@@ -216,21 +212,7 @@ class Ai:
                         2,
                     )
             # Blink during 7 seconds by retarding the end_signal call
-            # Event().wait(7)
-            # stateMachinePanel.end_signal(gpio_led)
-        else:
-            timer = threading.Timer(10, stop_blinking, [gpio_led])
-            timer.start()
+            Event().wait(7)
+            stateMachinePanel.end_signal(gpio_led)
 
         return img
-
-
-def stop_blinking(gpio_led):
-    """Thread routine to stop the led flashing.
-
-    Parameters
-    ----------
-    gpio_led : [int]
-        The GPIO pin
-    """
-    stateMachinePanel.end_signal(gpio_led)
